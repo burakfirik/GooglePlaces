ebbf803e4f18213c5e66d030f9c1c699253aaeed
burak firik <burakfirik@gmail.com>
2013-11-09 06:48:40 -0500

    JSON file parsing is implemented

--- GooglePlaces/ViewController.m
+++ GooglePlaces/ViewController.m
@@ -17,6 +17,40 @@
 - (void)viewDidLoad
 {
     [super viewDidLoad];
+    int radius=1000;
+    core_longitude=locationManager.location.coordinate.longitude;
+    core_latitude=locationManager.location.coordinate.latitude;
+    
+    self.myLawyerData=[NSMutableArray new];
+    
+    NSString *type=@"lawyer";
+    NSString *apiURL=[NSString stringWithFormat:@"https://maps.googleapis.com/maps/api/place/search/json?location=%f,%f&radius=%d&types=%@&sensor=true&key=AIzaSyAj9U6SK4yxy0eEry0_KtiFyY-lCp-mOXU",core_longitude, core_latitude, radius, type];
+    
+    NSURL *url= [NSURL URLWithString:apiURL];
+    
+    NSData *data=[NSData dataWithContentsOfURL:url];
+    NSError *error=[[NSError alloc] init];
+    NSMutableDictionary *myLawyerDict=[NSJSONSerialization JSONObjectWithData:data options:NSJSONReadingAllowFragments error:&error];
+    
+    NSMutableArray *resultsMA=[myLawyerDict objectForKey:@"results"];
+    
+    for(int i=0;i<[resultsMA count];i++){
+        
+        NSMutableDictionary *dict=[[[resultsMA objectAtIndex:i] objectForKey:@"geometry"] objectForKey:@"location"];
+        
+        NSString *latitude = [dict objectForKey:@"lat"];
+        NSString *longitude = [dict objectForKey:@"lng"];
+        NSString *name=[[resultsMA objectAtIndex:i] objectForKey:@"name"];
+        NSString *iconLink=[[resultsMA objectAtIndex:i] objectForKey:@"icon"];
+        NSString *address =[[resultsMA objectAtIndex:i] objectForKey:@"vicinity"];
+        
+        NSDictionary *tempDict=@{@"lat":latitude, @"lng":longitude, @"name":name, @"iconLink":iconLink, @"address":address};
+        
+        [self.myLawyerData addObject:tempDict];
+        
+    }
+    
+    
 	// Do any additional setup after loading the view, typically from a nib.
 }
 
